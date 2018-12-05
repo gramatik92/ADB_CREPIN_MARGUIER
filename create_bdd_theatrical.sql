30ef48b0126bf6a7bad155a6f339e2a57a21912a
graph <jumbococktail@gmail.com>
2018-12-05 23:56:40 +0100

    change of code

--- create_bdd_theatrical.sql
+++ create_bdd_theatrical.sql
@@ -1,11 +1,11 @@
-#------------------------------------------------------------
-#        Script MySQL - Advanced Database
-#   Made by Paul William CREPIN and Timothée Marguier
-#------------------------------------------------------------
+-----------------------------------------------------------
+--        Script MySQL - Advanced Database
+--  Made by Paul William CREPIN and Timothée Marguier
+-------------------------------------------------------------
 
-#------------------------------------------------------------
-# Table: SUPPRESSION des tables déjà existante!
-#------------------------------------------------------------
+-------------------------------------------------------------
+-- Table: SUPPRESSION des tables déjà existante!
+-------------------------------------------------------------
 DROP TABLE Network CASCADE CONSTRAINTS;
 DROP TABLE Company CASCADE CONSTRAINTS;
 DROP TABLE Produces_cost CASCADE CONSTRAINTS;
@@ -16,63 +16,59 @@ DROP TABLE Representation CASCADE CONSTRAINTS;
 DROP TABLE Grants CASCADE CONSTRAINTS;
 DROP TABLE Host CASCADE CONSTRAINTS;
 
-#------------------------------------------------------------
-#       Création des tables de la bd theatrical 
-#------------------------------------------------------------
-# Table: Network
-#------------------------------------------------------------
-
-CREATE TABLE Network(
-        network_id Int  Auto_increment  NOT NULL primary key
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Company
-#------------------------------------------------------------
-
-CREATE TABLE Company(
-        company_id  Int  Auto_increment  NOT NULL primary key ,
-        network_id  Int  Auto_increment  NOT NULL ,
-        room        Varchar (50) NOT NULL ,
-        productions Varchar (50) NOT NULL ,
-        budget      Int (10) NOT NULL
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Produces_cost
-#------------------------------------------------------------
-
-CREATE TABLE Produces_cost(
-        company_id      Int (10) NOT NULL ,
-        show_id         Int (10) NOT NULL ,
-        production_cost Int (10) NOT NULL
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Shows
-#------------------------------------------------------------
-
-CREATE TABLE Shows (
-    show_id Int(10) primary key
+------------------------------------------------------------
+--       Création des tables de la bd theatrical
+------------------------------------------------------------
+-- Table: Network
+------------------------------------------------------------
+CREATE TABLE Network
+(
+    network_id number (10) primary key
 );
 
-PROMPT
-pause Tapez sur Enter...
-PROMPT
+-----------------------------------------------------------
+-- Table: Company
+-------------------------------------------------------------
 
-#------------------------------------------------------------
-# Table: Theater
-#------------------------------------------------------------
+CREATE TABLE Company(
+        company_id  number(10) primary key ,
+        network_id  number(10),
+        productions Varchar (50),
+        room        Varchar (50),
+        budget      number (10)
+        foreign key(network_id) references Networks(network_id)
+)ENGINE=InnoDB;
+
+-------------------------------------------------------------
+-- Table: Shows
+-------------------------------------------------------------
+CREATE TABLE Shows (
+    show_id number(10) primary key
+)ENGINE=InnoDB;
+
+-------------------------------------------------------------
+-- Table: Produces_cost
+-------------------------------------------------------------
+
+CREATE TABLE Produces_cost(
+        company_id      number (10),
+        show_id         number (10),
+        production_cost number (10)
+        foreign key(company_id) REFERENCES Company(company_id),
+        foreign key(show_id) references Shows(show_id)
+)ENGINE=InnoDB;
+
+------------------------------------------------------------
+-- Table: Theater
+-------------------------------------------------------------
 
 CREATE TABLE Theater(
-        Theater_id       Int (10) Auto_increment  NOT NULL primary key ,
-        company_id       Int (10) NOT NULL ,
-        performence_hall Varchar (10) NOT NULL ,
-        fixed_capacity   Int (10),
-        balance          Int (10),
+        Theater_id       number (10) primary key ,
+        company_id       number (10),
+        fixed_capacity   number (10),
+        performence_hall Varchar (10),
+        balance          number (10),
+        foreign key (company_id) references Company(company_id)
 )ENGINE=InnoDB;
 
 
@@ -116,13 +112,23 @@ CREATE TABLE Grants(
 )ENGINE=InnoDB;
 
 
-#------------------------------------------------------------
-# Table: Host
-#------------------------------------------------------------
+-------------------------------------------------------------
+-- Table: Host
+-------------------------------------------------------------
 
 CREATE TABLE Host(
-        theater_id   Int(10) NOT NULL ,
-        show_id      Int(10) NOT NULL ,
-        global_price Int(10) NOT NULL ,
-        dates        Date NOT NULL
+        theater_id   number (10),
+        show_id      number (10),
+        global_price number (10),
+        dates        Date,
+        foreign key (theater_id) references Theater(theater_id),
+        foreign key (show_id) references Shows(show_id)
+)ENGINE=InnoDB;
+-------------------------------------------------------------
+-- Table: Dates of show
+-------------------------------------------------------------
+
+create table Dates
+(
+    dates number(7) primary key
 )ENGINE=InnoDB;
