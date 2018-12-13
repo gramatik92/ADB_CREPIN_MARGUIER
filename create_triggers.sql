e3e9cb7f1b0462d8e0b9f585804f073bef414414
graph <jumbococktail@gmail.com>
2018-12-13 15:47:28 +0100

    triggers à jour

--- create_triggers.sql
+++ create_triggers.sql
@@ -1,9 +1,85 @@
-DROP TRIGGER travel_cost;
+-----------------------------------------------------------
+--        Script - Advanced Database
+--  Made by Paul William CREPIN and Timothée Marguier
+-------------------------------------------------------------
+
+-------------------------------------------------------------
+-- Création de 5 triggers
+-- Pour ajouter une réduction
+-- Vérifier la promotion
+-- coup du ticket si les sièges sont disponibles
+-- coup de déplacement, si on a un cout ou non
+-- Nom de la personne
+-------------------------------------------------------------
+DROP TRIGGER discount_add;
+DROP TRIGGER discount_verif;
 DROP TRIGGER ticket_cost;
+DROP TRIGGER cout_travel;
 DROP TRIGGER booking_username;
 
+---------------------------------------
+-- Si un ticket possède reduced_reference_rate avec un de ces critères, il aura une réduction de 20%
+---------------------------------------
+CREATE TRIGGER discount_add
+before insert on Ticket
+for each row
+begin
+    SELECT t.reduced_reference_rate FROM Ticket t
+    if ((:t.reduced_reference_rate!='children') or  (:t.reduced_reference_rate!='students') or (:t.reduced_reference_rate!='unemployed') or (:new.reduced_reference_rate!='elderly')) then
+-- permet de générer une ligne entière dans le tampon. Un caractère fin de ligne est automatiquement ajouté en fin de ligne
+        t.price = t.price-(1*20/100);
+        dbms_output.put_line('Price reducted');
+    end if;
+end;
+/
 
-CREATE TRIGGER travel_cost
+---------------------------------------
+-- Si les discounts sont différentes de nos critères alors ça retourne une érreure
+---------------------------------------
+CREATE TRIGGER discount_verif
+Declare
+Select * From Ticket
+before insert on Ticket
+
+FOR EACH ROW
+BEGIN
+    if ((t.reduced_reference_rate ='children') or  (reduced_reference_rate='students') or (reduced_reference_rate='unemployed') or (reduced_reference_rate ='elderly')) then
+-- permet de générer une ligne entière dans le tampon. Un caractère fin de ligne est automatiquement ajouté en fin de ligne
+        dbms_output.put_line('erreur');
+    end if;
+end;
+/
+
+------------------------------------------
+-- selectionner un siège si il est disponible ou non
+------------------------------------------
+CREATE TRIGGER ticket_cost
+BEFORE CREATE on Ticket
+
+Declare
+seat boolean;
+Begin
+ where exists (
+  select seat_number INTO seat FROM Performance_Seat
+   then 1
+    else 0
+    end  into result
+  from dual;
+
+   if result = 1
+     then
+     --- Si c'est disponible, un message est renvoyé possible
+       DBMS_OUTPUT.put_line('SEAT AVAILABLE');
+     else
+          --- Si c'est indisponible, un message est renvoyé
+       DBMS_OUTPUT.put_line('SEAT ALREADY TAKEN');
+     end if;
+
+---------------------------------------
+-- Vérification cout de travel
+---------------------------------------
+
+CREATE TRIGGER cout_travel
 BEFORE UPDATE on Representation -- avant mise à jour
   FOR EACH ROW  -- pour chaque ligne
 Declare
@@ -23,28 +99,7 @@ Begin
   end if;
 End ;
 /
-------------------------------------------
--- selectionner un siège si il est disponible ou non
-------------------------------------------
-CREATE TRIGGER ticket_cost
-BEFORE CREATE on Ticket
 
-Declare
-seat boolean;
-Begin
- where exists (
-  select seat_number INTO seat FROM All_performance_seats
-   then 1
-    else 0
-    end  into result
-  from dual;
-
-   if result = 1
-     then
-       DBMS_OUTPUT.put_line('SEAT AVAILABLE');
-     else
-       DBMS_OUTPUT.put_line('SEAT ALREADY TAKEN');
-     end if;
 ------------------------------------------
 -- Ajout d'un utilisateur la table
 ------------------------------------------
@@ -52,32 +107,16 @@ CREATE TRIGGER booking_username
 AFTER INSERT
     ON Customers
     FOR EACH ROW
-
 DECLARE
         v_username varchar2(10);
      BEGIN
-
         -- Find username of person performing INSERT into table
         SELECT user INTO v_username
         FROM dual;
-
         -- Update create_date field to current system date
         :new.create_date := date;
-
         -- Update created_by field to the username of the person performing the INSERT
         :new.created_by := v_username;
 
      END;
-/
-
----------------------------------------
-
-create trigger discount_check
-before insert on Ticket
-for each row
-begin
-    if ((:new.reference_rate!='normal') or  (:new.reference_rate!='reduced')) then
-        dbms_output.put_line('ERROR');
-    end if;
-end;
 /
